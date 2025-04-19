0d3bbc14de9078604ff57db218b39b3648253746
Asile Ayuba <asileayuba@gmail.com>
2025-04-19 18:55:46 +0100

    Ready for Launch

--- core/models.py
+++ core/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils.text import slugify
+from cloudinary.models import CloudinaryField
 
 
 
 class Sector(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
-    logo = models.ImageField(upload_to='sector_logos/', blank=True, null=True)
+    logo = CloudinaryField('logo', null=True, blank=True)
     
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -22,7 +23,7 @@ class Startup(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     founding_date = models.DateField()
-    logo = models.ImageField(upload_to='logos/')
+    logo = CloudinaryField('logo', null=True, blank=True)
     sectors = models.ForeignKey(Sector, on_delete=models.CASCADE)
     
     def __str__(self):
--- launchpad/settings.py
+++ launchpad/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "core",
     "fontawesomefree",
+    "cloudinary_storage",
+    "cloudinary",
 ]
 
 MIDDLEWARE = [
